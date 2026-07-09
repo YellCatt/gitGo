181393b9605e5e0e5842f152bf31602a97bafb67
zs <768305875@qq.com>
2026-07-09 21:30:21 +0800

    24

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
+	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
 
